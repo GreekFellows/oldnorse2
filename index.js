2b987e615888bad109fc9ab8cd7d7d72cbdd204a
Greek Fellows <some.greek.fellows@hotmail.com>
2015-09-05 02:54:40 +0900

    index.js fix, minimized entry generation string

--- index.js
+++ index.js
@@ -22,50 +22,7 @@ $(document).ready(function() {
 		}
 		defString += "</ol>";
 
-		var divEntry = $("<div class=\"panel panel-default\">
-							<div class=\"panel-heading\">Result for: <i>\"" + searchText + "\"</i></div>
-							<div class=\"panel-body\">
-								<h2>" + entry.declension.singular.nominative + "</h2>
-
-								<p>
-									<i>n. " + entry.gender + "</i> " + (entry.strong ? "strong" : "weak") + ", " + entry.decltype + ".
-
-									" + defString + "
-								</p>
-
-								<table class=\"table\">
-									<thead>
-										<tr>
-											<th>#</th>
-											<th>singular</th>
-											<th>plural</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<th>nom.</th>
-											<td>"entry.declension.singular.nominative"</td>
-											<td>"entry.declension.plural.nominative"</td>
-										</tr>
-										<tr>
-											<th>gen.</th>
-											<td>"entry.declension.singular.genitive"</td>
-											<td>"entry.declension.plural.genitive"</td>
-										</tr>
-										<tr>
-											<th>dat.</th>
-											<td>"entry.declension.singular.dative"</td>
-											<td>"entry.declension.plural.dative"</td>
-										</tr>
-										<tr>
-											<th>acc.</th>
-											<td>"entry.declension.singular.accusative"</td>
-											<td>"entry.declension.plural.accusative"</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>");
+		var divEntry = $("<div class=\"panel panel-default\"><div class=\"panel-heading\">Result for: <i>\"" + searchText + "\"</i></div><div class=\"panel-body\"><h2>" + entry.declension.singular.nominative + "</h2><p><i>n. " + entry.gender + "</i> " + (entry.strong ? "strong" : "weak") + ", " + entry.decltype + "." + defString + "</p><table class=\"table\"><thead><tr><th>#</th><th>singular</th><th>plural</th></tr></thead><tbody><tr><th>nom.</th><td>"entry.declension.singular.nominative"</td><td>"entry.declension.plural.nominative"</td></tr><tr><th>gen.</th><td>"entry.declension.singular.genitive"</td><td>"entry.declension.plural.genitive"</td></tr><tr><th>dat.</th><td>"entry.declension.singular.dative"</td><td>"entry.declension.plural.dative"</td></tr><tr><th>acc.</th><td>"entry.declension.singular.accusative"</td><td>"entry.declension.plural.accusative"</td></tr></tbody></table></div></div>");
 		return divEntry;
 	}
 

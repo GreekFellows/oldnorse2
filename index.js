6a0fb070766a254b45119a9ba32ef12ff3b871f8
Greek Fellows <some.greek.fellows@hotmail.com>
2015-09-05 00:46:35 +0900

    updated index.js, testing search function

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
+	var nounsJSONLoaded = false;
+	var allJSONLoaded = false;
+
 	var nouns;
 
 	var nounsJSONPath = "./dictionary/nouns.json";
 
 	$.getJSON(nounsJSONPath, function(data) {
 		nouns = data;
-		console.log(JSON.stringify(nouns));
+		nounsJSONLoaded = true;
+
+		if (nounsJSONLoaded) {
+			allJSONLoaded = true;
+		}
+	});
+
+	$("#searchgo").click(function() {
+		if (allJSONLoaded) {
+			// first search in nouns
+			var searchText = $("#searchinput").val();
+			if (nouns.hasOwnProperty(searchText)) {
+				var newDiv = $("<div class=\"panel panel-default\"><div class=\"panel-body\"><div class=\"page-header\"><h1>" + searchText + "</h1></div><p>" + nouns[searchText].def + "</p></div></div>");
+				newDiv.appendTo("#searchresults");
+			}
+		}
 	});
 });
